d57189a53c414b02ceebfd8dddaf276c8441beab
Lukasz Gaza <lgaza@lgaza.kraklab.pl.ibm.com>
2015-04-18 23:25:30 +0200

    Parsed csv file

--- src/main/java/AvroSchema.java
+++ src/main/java/AvroSchema.java
@@ -3,10 +3,28 @@ import java.nio.file.Paths;
 import java.nio.file.Files;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.JsonNode;
+import java.util.Set;
+import java.util.HashSet;
 
+// TODO 
+// handle aliases
+// handle null values
+// handle enums and symbols
+// think about optional elements
+// how to understand that:
+//      {"name": "additional_image_link", "type": [{ "type": "array", "items": "string" }, "null"]},
+// how to understand that
+//        {"name": "condition", "type": {
+//      "type":"enum", "name":"condition_values", "symbols":["new","used"], "default": "new"}
+
+// handle different order of items
 public class AvroSchema {
 
   private String className;
+  // keep fields in hash collection for performance reason
+  // so looking for fields (while checking if they exist in csv file)
+  // is in constant time, not in e.g. linear time
+  private Set<String> fields = new HashSet<String>();
 
   public AvroSchema(String schemaFile) throws IOException {
     // read JSON file
@@ -18,11 +36,21 @@ public class AvroSchema {
     // read JSON like DOM parser
     JsonNode rootNode = objectMapper.readTree(jsonData);
     JsonNode nameNode = rootNode.path("name");
-
     className = nameNode.asText();
+
+    JsonNode fieldsNode = rootNode.path("fields");
+    if (fieldsNode.isArray()) {
+      for (JsonNode field: fieldsNode) {
+        fields.add(field.path("name").asText());
+      }
+    }
   }
 
   public String getClassName() {
     return className;
+  }
+
+  public Set<String> getFields() {
+    return fields;
   }
 }
--- src/main/java/CsvToAvro.java
+++ src/main/java/CsvToAvro.java
@@ -1,13 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.Map;
+import java.util.TreeMap;
+import java.io.File;
 
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.Schema;
 
 
 public class CsvToAvro {
-  private static String schema = "/home/lgaza/sandbox/gabor/csvtoavro/products.avsc";
+  private static String schemafile = "/home/lgaza/sandbox/gabor/csvtoavro/products.avsc";
+
+
 
   public static void main(String [] args) {
     try {
-      AvroSchema as = new AvroSchema(schema);
+      AvroSchema as = new AvroSchema(schemafile);
       System.out.println(as.getClassName());
+
+      BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+      String line = in.readLine();
+      String [] csvHeaders;
+      if (line != null) {
+        csvHeaders = line.split(",");
+      } else {
+        System.out.println("Input is empty");
+        return;
+      }
+
+      // to keep the order
+      Map<String, Boolean> headers = new TreeMap<String, Boolean>();
+      Set<String> avroFields = as.getFields();
+
+      if (csvHeaders != null) {
+        for (String s: csvHeaders) {
+          if (avroFields.contains(s)) {
+            headers.put(s, true);
+          } else {
+            headers.put(s, false);
+          }
+        }
+      }
+
+      Schema schema = new Schema.Parser().parse(new File(schemafile));
+      DatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>(schema);
+      //String s;
+      //while ((s = in.readLine()) != null && s.length() != 0) {
+      //  System.out.println(s);
+      //}
     } catch(Exception ex) {
       ex.printStackTrace();
     }
